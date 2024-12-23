2e221e13c709a25ae7f6f00cc5b2f12fdb5af439
florati25 <146290824+florati25@users.noreply.github.com>
2024-12-24 00:37:10 +0100

    florati test
    Création des modules nodes et du composant Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '' , component: HomeComponent
+    }
+];
+
+export const appRoutes = provideRouter(routes) //Exportation du model de routes
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {appRoutes} from './app/app.routes' //Appel de la route dans le fichier principal
+
+//Fichier principal
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
